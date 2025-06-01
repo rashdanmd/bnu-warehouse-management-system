7592497c6d1d81167bba065b2af4a1e574d22ae6
rashdanmd <rashdan.developer@gmail.com>
2025-06-01 12:07:37 +0100

    feat: initial supplier class

---
+++ src/models/Supplier/index.ts
@@ -0,0 +1,26 @@
+export class Supplier {
+  constructor(
+    public id: string,
+    public name: string,
+    public contactEmail: string
+  ) {
+    if (!Supplier.isValidEmail(contactEmail))
+      throw new Error("Not a valid email address");
+  }
+
+  public getSupplierDetails(): string {
+    return `Supplier ID: ${this.id}, Name: ${this.name}, Contact: ${this.contactEmail}`;
+  }
+
+  public updateContactEmail(newEmail: string): void {
+    if (!Supplier.isValidEmail(newEmail))
+      throw new Error("Not a valid email address");
+
+    this.contactEmail = newEmail;
+  }
+
+  public static isValidEmail(email: string): boolean {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+  }
+}
